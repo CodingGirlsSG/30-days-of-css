18ed08049303ce6c60f7eb5d3c8a08e166fd9033
Wei Gao <wgao19@gmail.com>
2019-02-08 21:06:22 +0800

    Filter future posts

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,44 +1,62 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import { parse } from 'query-string';
 
-import Intro from '../components/Intro'
-import Layout from '../components/Layout'
-import SEO from '../components/seo'
-import './style.css'
+import Intro from '../components/Intro';
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+import './style.css';
+
+const excludeFilterPosts = filterExistingPost => ({ node }) => {
+  if (!filterExistingPost) return true;
+  const dateInString = node && node.frontmatter && node.frontmatter.date;
+  return new Date(dateInString) < +new Date();
+};
+
+const VIEW_MODE = {
+  DEFAULT: 'exclude-future',
+  ALL: 'all',
+};
 
 class BlogIndex extends React.Component {
   render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
+    const {
+      data,
+      location: { search },
+    } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allMarkdownRemark.edges;
+    const { view } = parse(search);
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO keywords={[`css`, `coding girls`, `learning`, `coding`]} />
         <Intro />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h2 className="title">
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h2>
-              <small>{node.frontmatter.date}</small>
-              <p
-                className="excerpt"
-                dangerouslySetInnerHTML={{ __html: node.excerpt }}
-              />
-            </div>
-          )
-        })}
+        {posts
+          .filter(excludeFilterPosts(view !== VIEW_MODE.ALL))
+          .map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug;
+            return (
+              <div key={node.fields.slug}>
+                <h2 className="title">
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h2>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  className="excerpt"
+                  dangerouslySetInnerHTML={{ __html: node.excerpt }}
+                />
+              </div>
+            );
+          })}
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -62,4 +80,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
