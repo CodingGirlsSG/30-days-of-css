2fdec81872a8142c2bae12cde35e69f11c5e7cd7
Wei Gao <wgao19@gmail.com>
2019-02-23 18:07:57 +0800

    Update mentors styles for mobile

--- site/src/pages/mentors/index.js
+++ site/src/pages/mentors/index.js
@@ -20,58 +20,56 @@ class Mentors extends React.Component {
           {mentors.map((mentor, idx) => (
             <React.Fragment key={`mentor-${idx}`}>
               <img src={mentor.avatar} className="avatar" />
-              <div className="mentor-intro">
-                <span className="mentor-profile">
-                  {mentor.website ? (
-                    <a
-                      href={mentor.website}
-                      target="_blank"
-                      className="mentor-name"
-                    >
-                      {mentor.name}
-                    </a>
-                  ) : (
-                    <span className="mentor-name">{mentor.name}</span>
-                  )}
-                  {mentor.twitter && (
-                    <a
-                      target="_blank"
-                      href={`https://twitter.com/${mentor.twitter}`}
-                      className="logo-link"
-                    >
-                      <img
-                        width="16"
-                        src="https://cdn.worldvectorlogo.com/logos/twitter.svg"
-                      />
-                    </a>
-                  )}
-                  {mentor.github && (
-                    <a
-                      target="_blank"
-                      href={`https://github.com/${mentor.github}`}
-                      className="logo-link"
-                    >
-                      <img
-                        width="16"
-                        src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg"
-                      />
-                    </a>
-                  )}
-                  {mentor.codepen && (
-                    <a
-                      target="_blank"
-                      href={`https://codepen.io/${mentor.codepen}`}
-                      className="logo-link"
-                    >
-                      <img
-                        width="16"
-                        src="http://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png"
-                      />
-                    </a>
-                  )}
-                </span>
-                <p>{mentor.intro}</p>
+              <div className="mentor-links">
+                {mentor.website ? (
+                  <a
+                    href={mentor.website}
+                    target="_blank"
+                    className="mentor-name"
+                  >
+                    {mentor.name}
+                  </a>
+                ) : (
+                  <span className="mentor-name">{mentor.name}</span>
+                )}
+                {mentor.twitter && (
+                  <a
+                    target="_blank"
+                    href={`https://twitter.com/${mentor.twitter}`}
+                    className="logo-link"
+                  >
+                    <img
+                      width="16"
+                      src="https://cdn.worldvectorlogo.com/logos/twitter.svg"
+                    />
+                  </a>
+                )}
+                {mentor.github && (
+                  <a
+                    target="_blank"
+                    href={`https://github.com/${mentor.github}`}
+                    className="logo-link"
+                  >
+                    <img
+                      width="16"
+                      src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg"
+                    />
+                  </a>
+                )}
+                {mentor.codepen && (
+                  <a
+                    target="_blank"
+                    href={`https://codepen.io/${mentor.codepen}`}
+                    className="logo-link"
+                  >
+                    <img
+                      width="16"
+                      src="http://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png"
+                    />
+                  </a>
+                )}
               </div>
+              <p className="mentor-intro">{mentor.intro}</p>
             </React.Fragment>
           ))}
         </div>
