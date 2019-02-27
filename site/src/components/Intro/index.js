9682c65a67c93127de4bc84e9fe2588168a97fa5
Wei Gao <wgao19@gmail.com>
2019-02-27 15:56:15 +0800

    Update navigation links and style

--- site/src/components/Intro/index.js
+++ site/src/components/Intro/index.js
@@ -9,34 +9,45 @@ function Bio() {
       render={data => {
         const { author, social } = data.site.siteMetadata;
         return (
-          <>
-            <div className="intro">
-              Join a 30-day CSS challenge with{' '}
-              <a href="http://codinggirls.sg/" target="_blank">
-                CodingGirls
-              </a>
-              !!!
-            </div>
-            <div>
-              <a
-                href="https://goo.gl/forms/ZQCns05vrHjMqt662"
-                target="_blank"
-                className="sign-up-link"
-              >
-                Sign Up
-              </a>
-              <a
-                href="https://codepen.io/collection/XJJoBY/"
-                target="_blank"
-                className="sign-up-link"
-              >
-                Challenge Collection
-              </a>
-              <a href="/mentors" className="sign-up-link">
-                Meet Our Mentors
-              </a>
-            </div>
-          </>
+          <div className="intro-links">
+            <a
+              href="https://codepen.io/collection/XJJoBY/"
+              target="_blank"
+              className="intro-link"
+            >
+              Challenge Collection
+            </a>
+            <a
+              href="https://codepen.io/collection/XmwwGK/"
+              target="_blank"
+              className="intro-link"
+            >
+              Show Room
+            </a>
+            <a href="/mentors" className="intro-link">
+              Meet Our Mentors
+            </a>
+            <a
+              target="_blank"
+              href="https://twitter.com/hashtag/30DaysOfCSSGirls"
+              className="logo-link"
+            >
+              <img
+                width="16"
+                src="https://cdn.worldvectorlogo.com/logos/twitter.svg"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://codepen.io/tag/30DaysOfCSSGirls"
+              className="logo-link"
+            >
+              <img
+                width="16"
+                src="http://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Large.png"
+              />
+            </a>
+          </div>
         );
       }}
     />
